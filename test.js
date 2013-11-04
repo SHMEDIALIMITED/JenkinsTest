c140bea72198a2df9ba59f5a550e18ed60d06110
PatrickWolleb <patrick.wolleb@shmedia.co.uk>
2013-11-04 12:26:07 -0800

    Testing

--- test.js
+++ test.js
@@ -2,4 +2,3 @@ console.log('HELLO JENKINS');
 console.log('HELLO JENKINS');
 console.log('HELLO JENKINS');
 console.log('HELLO TETSTSTSTTST');
-console.log('HELLO TETSTSTSTTST');console.log('HELLO TETSTSTSTTST');console.log('HELLO TETSTSTSTTST');console.log('HELLO TETSTSTSTTST');console.log('HELLO TETSTSTSTTST');
